8710c4ebdf4f987b26b4b7cf1b9ddd8509c39c08
Makoto Takahashi <quanty@wa2.so-net.ne.jp>
2014-11-29 10:37:28 +0900

    Update batman_eq_randam.r
    最新のR言語環境で動作するように修正した。

--- R/batman_eq_randam.r
+++ R/batman_eq_randam.r
@@ -1,46 +1,30 @@
+install.packages("ggplot2")
 require(ggplot2)
-
 x <- runif(500000, -10, 10)
 y <- runif(500000, -4, 4)
-
 f1 <- ((x/7)^2)*sqrt((abs(abs(x) - 3))/(abs(x) - 3)) + ((y/3)^2)*sqrt((abs(y + (3*sqrt(33))/7))/(y + (3*sqrt(33))/7)) - 1
-
 f2 <- abs(x/2) - ((3*sqrt(33) - 7)/112)*x^2 - 3 + sqrt(1 - (abs(abs(x) - 2) - 1)^2) - y
-
 f3 <- 9*sqrt(abs((abs(x) - 1)*(abs(x) - 3/4))/(1 - abs(x))/(abs(x) - 3/4)) - 8*abs(x) - y
-
 f4 <- 3*abs(x) + (3/4)*sqrt(abs((abs(x) - 3/4)*(abs(x) - 1/2))/(3/4 - abs(x))/(abs(x) - 1/2)) - y
-
 f5 <- (9/4)*sqrt(abs((x - 1/2)*(x + 1/2))/(1/2 - x)/(1/2 + x)) - y
-
 f6 <- (6*sqrt(10))/7 + (3/2 - abs(x)/2)*sqrt((abs(abs(x) - 1))/(abs(x) - 1)) - ((6*sqrt(10))/14) *sqrt(4 - (abs(x) - 1)^2 ) - y
-
-d1 <- data.frame(x=x,y=y,f=f1)
+d1 <- data.frame(x=x,y=y,f=f1,col="red")
 dd1 <- subset(d1, d1$f<0.025 & d1$f>-0.025 )
-p1 <- ggplot(dd1,aes(x,y)) + geom_point(color="red")
-
-d2 <- data.frame(x=x,y=y,f=f2)
+d2 <- data.frame(x=x,y=y,f=f2,col="yellow")
 dd2 <- subset(d2, d2$f<0.025 & d2$f>-0.025 )
-p2 <- p1+geom_point(aes(x=dd2$x,y=dd2$y), color="yellow")
-
-d3 <- data.frame(x=x,y=y,f=f3)
+d3 <- data.frame(x=x,y=y,f=f3,col="green")
 dd3 <- subset(d3, d3$f<0.05 & d3$f>-0.05 )
-p3 <- p2+geom_point(aes(x=dd3$x,y=dd3$y), color="green")
-
-d4 <- data.frame(x=x,y=y,f=f4)
+d4 <- data.frame(x=x,y=y,f=f4,col="steelblue")
 dd4 <- subset(d4, d4$f<0.025 & d4$f>-0.025 )
-p4 <- p3+geom_point(aes(x=dd4$x,y=dd4$y), color="steelblue")
-
-d5 <- data.frame(x=x,y=y,f=f5)
+d5 <- data.frame(x=x,y=y,f=f5,col="black")
 dd5 <- subset(d5, d5$f<0.025 & d5$f>-0.025 )
-p5 <- p4+geom_point(aes(x=dd5$x,y=dd5$y))
-
-d6 <- data.frame(x=x,y=y,f=f6)
+d6 <- data.frame(x=x,y=y,f=f6,col="blue")
 dd6 <- subset(d6, d6$f<0.05 & d6$f>-0.05 )
-p6 <- p5+geom_point(aes(x=dd6$x,y=dd6$y), color="blue")
 
-print(p6)
-
-#p4 <- p3+geom_point(aes(x=x4,y=y4), color="steelblue")
-#p5 <- p4+geom_point(aes(x=x5,y=y5))
-#p6 <- p5+geom_point(aes(x=x6,y=y6), colour="blue")
+r1 = rbind ( dd1, dd2)
+r1 = rbind ( r1, dd3)
+r1 = rbind ( r1, dd4)
+r1 = rbind ( r1, dd5)
+r1 = rbind ( r1, dd6)
+p1 <- ggplot(r1,aes(x,y,col)) + geom_point(col=r1$col)
+print(p1)
